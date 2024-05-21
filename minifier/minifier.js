dad968f56ab77f9bc6931576d18b6cc7f01a2c28
ItzNotABug <itznotabug@gmail.com>
2024-05-21 18:07:29 +0530

    feat: add content minifier middleware.

---
+++ minifier/minifier.js
@@ -0,0 +1,134 @@
+import { minify as htmlMinifier } from 'html-minifier-terser';
+
+const configOptions = { excludedPaths: [], htmlMinifierOptions: {} };
+
+export default {
+    /**
+     * Sets options for the middleware.
+     *
+     * @param {Object} options - Configuration options.
+     * @param {(string|RegExp)[]} [options.excludes=[]] - Paths to exclude.
+     * Supports strings and regular expressions.
+     * Minification won't be applied if a path matches any one in excluded paths.
+     * @param {Object} [options.htmlOptions={}] - HTML minifier options.
+     */
+    options: ({ excludes = [], htmlOptions = {} }) => {
+        configOptions.excludedPaths = excludes;
+        configOptions.htmlMinifierOptions = htmlOptions;
+    },
+
+    /**
+     * Middleware that minifies content.
+     *
+     * @param {Object} request - The `RequestHandler` object.
+     * @param {Object} interceptor - The `ResponseInterceptor` object.
+     * @param {function(string)} log - Function to log debug message.
+     * @param {function(string)} error - Function to log error message.
+     */
+    middleware: {
+        outgoing: async (request, interceptor, _, error) => {
+            try {
+                await checkAndMinify(request, interceptor);
+            } catch (err) {
+                error(`Unable to minify the content, ${err.message}`);
+            }
+        },
+    },
+};
+
+/**
+ * Check for excluded paths and minify the content if required.
+ *
+ * @param {Object} request - The `RequestHandler` object.
+ * @param {Object} interceptor - The `ResponseInterceptor` object.
+ */
+const checkAndMinify = async (request, interceptor) => {
+    const excludes = configOptions.excludedPaths;
+    const isExcluded = excludes.some((exclude) =>
+        typeof exclude === 'string'
+            ? exclude === request.path
+            : exclude.test(request.path),
+    );
+
+    if (isExcluded) return;
+
+    const { body, headers = {} } = interceptor;
+
+    // return if the content is marked for exclusion.
+    let excludeMinify = headers['exclude-minify'];
+    if (excludeMinify) {
+        // only numbers and strings allowed currently,
+        // should be addressed in the next release ig.
+        excludeMinify = excludeMinify?.toLowerCase() === 'true';
+    } else excludeMinify = false;
+
+    if (excludeMinify) return;
+
+    const isBuffer = Buffer.isBuffer(body);
+    if (isBuffer) return;
+
+    const contentType = headers['content-type'] ?? '';
+    if (
+        isJsonString(body) ||
+        typeof body === 'string' ||
+        contentType.startsWith('text/') ||
+        contentType === 'application/xml' ||
+        contentType === 'application/json'
+    ) {
+        await minify(interceptor);
+    }
+};
+
+/**
+ * Checks if the given string is a valid JSON format.
+ *
+ * @param {string} content - Content to check.
+ * @return {boolean} True if valid `JSON`, false otherwise.
+ */
+const isJsonString = (content) => {
+    try {
+        JSON.parse(content);
+    } catch (error) {
+        return false;
+    }
+
+    return true;
+};
+
+/**
+ * Minifies the given content.
+ *
+ * @param {Object} interceptor - Content to minify in the `ResponseInterceptor` object.
+ */
+const minify = async (interceptor) => {
+    let content = interceptor.body;
+    const isXml = content.includes('<xml');
+    const isHtml = content.includes('<html');
+
+    if (isJsonString(content)) {
+        try {
+            content = JSON.stringify(JSON.parse(content));
+            interceptor.body = content.trim();
+            return;
+        } catch (error) {
+            return;
+        }
+    }
+
+    if (isHtml) {
+        content = await htmlMinifier(
+            content,
+            configOptions.htmlMinifierOptions,
+        );
+    } else {
+        if (isXml) {
+            content = content.replace(/>([^<]+)</g, (match, key) => {
+                return '>' + key.replace(/\s+/g, ' ').trim() + '<';
+            });
+        } else {
+            content = content.replace(/\s+/g, ' ');
+        }
+    }
+
+    interceptor.body = content.trim();
+};
---
+++ minifier/tests/test.js
@@ -0,0 +1,163 @@
+import assert from 'assert';
+import minifier from '../minifier.js';
+import { describe, it } from 'node:test';
+
+/**
+ * html minifier options.
+ */
+const htmlMinifierOptions = {
+    minifyJS: true,
+    minifyCSS: true,
+    removeComments: true,
+    collapseWhitespace: true,
+    preserveLineBreaks: false,
+};
+
+/**
+ * create a pseudo-interceptor for tests.
+ */
+const createInterceptor = (content) => {
+    return { body: content, headers: {} };
+};
+
+/**
+ * run the minifier.
+ */
+const minify = async (interceptor, options = {}) => {
+    minifier.options({
+        htmlOptions: {
+            ...options,
+            ...htmlMinifierOptions,
+        },
+    });
+    await minifier.middleware.outgoing(
+        {},
+        interceptor,
+        console.log,
+        console.error,
+    );
+};
+
+describe(`Minification Test`, () => {
+    it(`basic minification`, async () => {
+        const expected =
+            '<html lang="en"><body><h1>AppExpress Minifier</h1><p>AppExpress minifier enables minifying the content for faster transfers!</p></body></html>';
+        const html = `
+            <html lang="en">
+                <body>
+                    <h1>AppExpress Minifier</h1>
+                    <p>AppExpress minifier enables minifying the content for faster transfers!</p>
+                </body>
+            </html>
+        `;
+
+        const interceptor = createInterceptor(html);
+        await minify(interceptor);
+
+        assert.strictEqual(interceptor.body, expected);
+    });
+
+    it(`basic minification with comments`, async () => {
+        const expected =
+            '<html lang="en"><body><h1>AppExpress Minifier</h1><p>AppExpress minifier enables minifying the content for faster transfers!</p></body></html>';
+        const html = `
+            <!-- this is a top level comment -->
+            <html lang="en">
+                <!-- this is a nested comment -->
+                <body>
+                    <!-- this is another nested comment -->
+                    <h1>AppExpress Minifier</h1>
+                    <p>AppExpress minifier enables minifying the content for faster transfers!</p>
+                </body>
+                <!-- this is a nested comment -->
+            </html>
+            <!-- this is a top level comment -->
+        `;
+
+        const interceptor = createInterceptor(html);
+
+        await minify(interceptor);
+
+        assert.strictEqual(interceptor.body, expected);
+    });
+
+    it(`basic minification with styles, scripts`, async () => {
+        const expected = `<html lang="en"><head><title>AppExpress Minifier</title><script>console.log("hi there, this is a demo log")</script><style>h1{text-decoration:underline}p{font-style:italic;font-family:serif}</style></head><body><h1>AppExpress Minifier</h1><p>AppExpress minifier enables minifying the content for faster transfers!</p></body></html>`;
+        const html = `
+            <!-- this is a top level comment -->
+            <html lang="en">
+                <head>
+                    <title>AppExpress Minifier</title>
+                    <script>console.log('hi there, this is a demo log');</script>
+                    <style>
+                        h1 {
+                            text-decoration: underline;
+                        }
+
+                        p {
+                            font-style: italic;
+                            font-family: serif;
+                        }
+                    </style>
+                </head>
+
+                <!-- this is a nested comment -->
+                <body>
+                    <!-- this is another nested comment -->
+                    <h1>AppExpress Minifier</h1>
+                    <p>AppExpress minifier enables minifying the content for faster transfers!</p>
+                </body>
+                <!-- this is a nested comment -->
+            </html>
+            <!-- this is a top level comment -->
+        `;
+
+        const interceptor = createInterceptor(html);
+
+        await minify(interceptor);
+
+        assert.strictEqual(interceptor.body, expected);
+    });
+
+    it(`basic minification with normal text`, async () => {
+        const expected = 'this is the first line, this is the second line!';
+        const textContent = `
+            this is the first line,
+            this is the second line!
+        `;
+
+        const interceptor = createInterceptor(textContent);
+
+        await minify(interceptor);
+
+        assert.strictEqual(interceptor.body, expected);
+    });
+
+    it(`basic minification with json text`, async () => {
+        const json = { body: 'someContent', headers: {}, statusCode: 200 };
+        const textContent = JSON.stringify(json, null, 4);
+
+        const interceptor = createInterceptor(textContent);
+
+        await minify(interceptor);
+
+        assert.strictEqual(interceptor.body, JSON.stringify(json));
+    });
+
+    it(`basic minification with xml content`, async () => {
+        const expected = `<xml><one>some content here</one></xml>`;
+        const xml = `
+            <xml>
+                <one>
+                    some content here
+                </one>
+            </xml>
+        `;
+
+        const interceptor = createInterceptor(xml, true);
+
+        await minify(interceptor);
+
+        assert.strictEqual(interceptor.body, expected);
+    });
+});
